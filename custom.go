9d11fa925810f00b40bc455ef5d3ca42e21fff6e
Andrew Phelps <ajp2455@vt.edu>
2020-04-01 09:59:28 -0400

    added window helper

--- custom.go
+++ custom.go
@@ -5,3 +5,8 @@ type Payload struct {
 	Clients    []*Client              `json:"clients,omitempty"`
 	ScriptData map[string]interface{} `json:"script,omitempty"`
 }
+
+func (w *Window) IsInsidePeriod(low, high int64) bool {
+	return ((w.Start > low && w.End < high) || (w.Start < high && w.Start > low) ||
+		(w.End < high && w.End > low) || ((low < w.End && low > w.Start) && (high < w.End && high > w.Start)))
+}
